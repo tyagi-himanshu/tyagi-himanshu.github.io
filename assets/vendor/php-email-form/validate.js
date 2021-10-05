9d3bde248a02c14a1a758e708b577686a16620c2
himanshutyagi713 <86161787+himanshutyagi713@users.noreply.github.com>
2021-10-05 13:25:27 +0530

    update1

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,7 +1,7 @@
 /**
 * PHP Email Form Validation - v3.1
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
+* URL: 
+* Author: Himanshu Tyagi
 */
 (function () {
   "use strict";
